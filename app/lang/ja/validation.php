e067e486d0418b260f64f333469d187adef1cbc6
Hirohisa Kawase <hirokws@gmail.com>
2013-07-31 10:43:08 +0900

    Added new items into validation ja lang file

--- app/lang/ja/validation.php
+++ app/lang/ja/validation.php
@@ -4,79 +4,79 @@ return array(
 
 	/*
 	|--------------------------------------------------------------------------
-	| Validation Language Lines
+	| バリデーション言語行
 	|--------------------------------------------------------------------------
 	|
-	| The following language lines contain the default error messages used by
-	| the validator class. Some of these rules have multiple versions such
-	| such as the size rules. Feel free to tweak each of these messages.
+	| 以下の言語行はバリデタークラスにより使用されるデフォルトのエラー
+	| メッセージです。サイズルールのようにいくつかのバリデーションを
+	| 持っているものもあります。メッセージはご自由に調整してください。
 	|
 	*/
 
-	"accepted"         => "The :attribute must be accepted.",
-	"active_url"       => "The :attribute is not a valid URL.",
-	"after"            => "The :attribute must be a date after :date.",
-	"alpha"            => "The :attribute may only contain letters.",
-	"alpha_dash"       => "The :attribute may only contain letters, numbers, and dashes.",
-	"alpha_num"        => "The :attribute may only contain letters and numbers.",
+	"accepted"         => ":attributeを承認してください。",
+	"active_url"       => ":attributeが有効なURLではありません。",
+	"after"            => ":attributeには、:date以降の日付を指定してください。",
+	"alpha"            => ":attributeはアルファベッドのみがご利用できます。",
+	"alpha_dash"       => ":attributeは英数字とダッシュ(-)及び下線(_)がご利用できます。",
+	"alpha_num"        => ":attributeは英数字がご利用できます。.",
 	"array"            => "The :attribute must be an array.",
-	"before"           => "The :attribute must be a date before :date.",
+	"before"           => ":attributeには、:date以前の日付をご利用ください。",
 	"between"          => array(
-		"numeric" => "The :attribute must be between :min - :max.",
-		"file"    => "The :attribute must be between :min - :max kilobytes.",
-		"string"  => "The :attribute must be between :min - :max characters.",
-		"array"   => "The :attribute must have between :min - :max items.",
+		"numeric" => ":attributeは、:minから、:maxまでの数字をご指定ください。",
+		"file"    => ":attributeには、:min kBから:max kBまでのサイズのファイルをご指定ください。",
+		"string"  => ":attributeは、:min文字から:max文字の間でご指定ください。",
+		"array"   => ":attributeは:minから:max個指定してください。",
 	),
-	"confirmed"        => "The :attribute confirmation does not match.",
-	"date"             => "The :attribute is not a valid date.",
-	"date_format"      => "The :attribute does not match the format :format.",
-	"different"        => "The :attribute and :other must be different.",
-	"digits"           => "The :attribute must be :digits digits.",
-	"digits_between"   => "The :attribute must be between :min and :max digits.",
-	"email"            => "The :attribute format is invalid.",
-	"exists"           => "The selected :attribute is invalid.",
-	"image"            => "The :attribute must be an image.",
-	"in"               => "The selected :attribute is invalid.",
-	"integer"          => "The :attribute must be an integer.",
-	"ip"               => "The :attribute must be a valid IP address.",
+	"confirmed"        => ":attributeと、確認フィールドとが、一致していません。",
+	"date"             => ":attributeには有効な日付を指定してください。",
+	"date_format"      => ":attributeは:format形式で指定してください。",
+	"different"        => ":attributeと:otherには、異なった内容を指定してください。",
+	"digits"           => ":attributeは:digits桁で指定してください。",
+	"digits_between"   => ":attributeは:min桁から:max桁の間で指定してください。",
+	"email"            => ":attributeに正しい形式をご指定ください。",
+	"exists"           => "選択された:attributeは正しくありません。",
+	"image"            => ":attributeには画像ファイルを指定してください。",
+	"in"               => "選択された:attributeは正しくありません。",
+	"integer"          => ":attributeは整数でご指定ください。",
+	"ip"               => ":attributeには、有効なIPアドレスをご指定ください。",
 	"max"              => array(
-		"numeric" => "The :attribute may not be greater than :max.",
-		"file"    => "The :attribute may not be greater than :max kilobytes.",
-		"string"  => "The :attribute may not be greater than :max characters.",
-		"array"   => "The :attribute may not have more than :max items.",
+		"numeric" => ":attributeには、:max以下の数字をご指定ください。",
+		"file"    => ":attributeには、:max kB以下のファイルをご指定ください。",
+		"string"  => ":attributeは、:max文字以下でご指定ください。",
+		"array"   => ":attributeは:max個以下ご指定ください。",
 	),
-	"mimes"            => "The :attribute must be a file of type: :values.",
+	"mimes"            => ":attributeには:valuesタイプのファイルを指定してください。",
 	"min"              => array(
-		"numeric" => "The :attribute must be at least :min.",
-		"file"    => "The :attribute must be at least :min kilobytes.",
-		"string"  => "The :attribute must be at least :min characters.",
-		"array"   => "The :attribute must have at least :min items.",
+		"numeric" => ":attributeには、:min以上の数字をご指定ください。",
+		"file"    => ":attributeには、:min kB以上のファイルをご指定ください。",
+		"string"  => ":attributeは、:min文字以上でご指定ください。",
+		"array"   => ":attributeは:min個以上ご指定ください。",
 	),
-	"not_in"           => "The selected :attribute is invalid.",
-	"numeric"          => "The :attribute must be a number.",
-	"regex"            => "The :attribute format is invalid.",
-	"required"         => "The :attribute field is required.",
-	"required_if"      => "The :attribute field is required when :other is :value.",
-	"required_with"    => "The :attribute field is required when :values is present.",
-	"required_without" => "The :attribute field is required when :values is not present.",
-	"same"             => "The :attribute and :other must match.",
+	"not_in"           => "選択された:attributeは正しくありません。",
+	"numeric"          => ":attributeには、数字を指定してください。",
+	"regex"            => ":attributeに正しい形式をご指定ください。",
+	"required"         => ":attributeは必ず指定してください。",
+	"required_if"      => ":otherが:valueの場合、:attributeも指定してください。",
+	"required_with"    => ":valuesを指定する場合は、:attributeも指定してください。",
+	"required_without" => ":valuesを指定しない場合は、:attributeを指定してください。",
+	"same"             => ":attributeと:otherには同じ値を指定してください。",
 	"size"             => array(
-		"numeric" => "The :attribute must be :size.",
-		"file"    => "The :attribute must be :size kilobytes.",
-		"string"  => "The :attribute must be :size characters.",
-		"array"   => "The :attribute must contain :size items.",
+		"numeric" => ":attributeは:sizeを指定してください。",
+		"file"    => ":attributeのファイルは、:sizeキロバイトでなくてはなりません。",
+		"string"  => ":attributeは:size文字で指定してください。",
+		"array"   => ":attributeは:size個ご指定ください。",
 	),
-	"unique"           => "The :attribute has already been taken.",
-	"url"              => "The :attribute format is invalid.",
+	"unique"           => ":attributeの値は既に存在しています。",
+	"url"              => ":attributeに正しい形式をご指定ください。",
 
 	/*
 	|--------------------------------------------------------------------------
-	| Custom Validation Language Lines
+	| カスタムバリデーション言語行
 	|--------------------------------------------------------------------------
 	|
-	| Here you may specify custom validation messages for attributes using the
-	| convention "attribute.rule" to name the lines. This makes it quick to
-	| specify a specific custom language line for a given attribute rule.
+	| "属性.ルール"の規約でキーを指定することでカスタムバリデーション
+	| メッセージを定義できます。指定した属性ルールに対する特定の
+	| カスタム言語行を手早く指定できます。
 	|
 	*/
 
@@ -84,12 +84,12 @@ return array(
 
 	/*
 	|--------------------------------------------------------------------------
-	| Custom Validation Attributes
+	| カスタムバリデーション属性名
 	|--------------------------------------------------------------------------
 	|
-	| The following language lines are used to swap attribute place-holders
-	| with something more reader friendly such as E-Mail Address instead
-	| of "email". This simply helps us make messages a little cleaner.
+	| 以下の言語行は、例えば"email"の代わりに「メールアドレス」のように、
+	| 読み手にフレンドリーな表現でプレースホルダーを置き換えるために指定する
+	| 言語行です。これはメッセージをよりきれいに表示するために役に立ちます。
 	|
 	*/
 
